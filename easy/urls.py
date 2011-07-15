8cf81be8bde0a7b05891e6c7c0cc204a885c95f1
lee <linkedlinked@gmail.com>
2011-07-15 13:14:19 -0700

    new add_params logic works even better

--- easy/urls.py
+++ easy/urls.py
@@ -1,28 +1,8 @@
-from urlparse import urlparse, urlunparse 
-from urllib import urlencode
+import urlparse, urllib
 
-def add_params(url, **params): 
-    """ don't just add the **params because the url 
-    itself might contain CGI variables embedded inside 
-    the string. """ 
-    url_parsed = list(urlparse(url))
-
-    encoded = urlencode(params) 
-    qs = url_parsed[4] 
-    if encoded: 
-        if qs: 
-            qs += '&'+encoded 
-        else: 
-            qs = encoded 
-    netloc = url_parsed[1] 
-    if netloc.find('?')>-1: 
-        url_parsed[1] = url_parsed[1][:netloc.find('?')] 
-        if qs: 
-            qs = netloc[netloc.find('?')+1:]+'&'+qs 
-        else: 
-            qs = netloc[netloc.find('?')+1:]
-
-    url_parsed[4] = qs
-
-    url = urlunparse(url_parsed) 
-    return url 
+def add_params(url, params): 
+    url_parts = list(urlparse.urlparse(url))
+    query = dict(urlparse.parse_qsl(url_parts[4]))
+    query.update(params)
+    url_parts[4] = urllib.urlencode(query)
+    return urlparse.urlunparse(url_parts)
